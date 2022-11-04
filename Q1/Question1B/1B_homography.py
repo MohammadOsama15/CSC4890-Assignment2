9ae501e4aa77dbda7d5b326d1036007a11ec738d
o5a3a5a3 <61632192+MohammadOsama15@users.noreply.github.com>
2022-11-04 12:41:08 -0400

    Assignment 2 files

---
+++ Q1/Question1B/1B_homography.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+src_points = np.array([[324,530],[179,717],[782,776],[761,525],[223,461] ])
+dest_points = np.array([[517,643],[185,632],[786,763],[452,601],[111,628]])
+
+
+h, status = cv2.findHomography(src_points, dest_points)
+print(h)
+im_src = cv2.imread('img1greyscale.png')
+im_dst = cv2.imread('img2grayscale.png')
+
+im_out = cv2.warpPerspective(im_src, h, (im_dst.shape[1],im_dst.shape[0]))
+
+cv2.imshow("Warped_Source_Image", im_out)
+plt.imshow(im_out)
+plt.show()
+
+#Coordinates for image1
+# 139   730
+# 123   318
+# 775   734
+# 148   75
+# 783   323
+
+
+
